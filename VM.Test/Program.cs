77ccc72668fa4cdc32a8b236c50cdf5ef914c531
niko <nikoranger96@gmail.com>
2024-09-22 21:46:10 +0800

    测试代码

--- VM.Test/Program.cs
+++ VM.Test/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using VM.Image;
+
 Console.WriteLine("Hello, World!");
+
+var files = ImageHelper.GetInfos(@"E:\相册");
+
+Console.ReadLine();
